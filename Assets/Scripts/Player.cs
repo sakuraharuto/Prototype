545bae573a92fa90925e066cea61d99f0047134b
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-02-04 17:50:13 -0500

    Player move left and right

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{
+{   
+
+    [SerializeField] float moveSpeed = 5f;
+    Rigidbody2D rb2d;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
-    {
-        
+    {   
+        float x = Input.GetAxis("Horizontal");
+        //float y = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = new Vector3(x, 0, 0);
+
+        moveVector = moveVector.normalized * moveSpeed * Time.deltaTime;
+
+        rb2d.MovePosition(rb2d.transform.position + moveVector);
+
+        // Use rigidbody instead
+        /*
+        if (Input.GetKey(KeyCode.RightArrow)){
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.LeftArrow)){
+            transform.position += Vector3.left *moveSpeed * Time.deltaTime;
+        }
+        */
     }
 }
