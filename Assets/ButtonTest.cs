4d20141caa03e730e1663d5194e59c2c66c286c8
RocketboyGhy <2783529117@qq.com>
2022-02-07 02:29:00 -0500

    restart

---
+++ Assets/ButtonTest.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
