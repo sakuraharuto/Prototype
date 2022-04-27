f9f8eed47f75ed7f4f497e7834e8c4e38c51e851
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-04-27 17:28:58 -0400

    Repeated SFX bug fixed

--- Assets/Scripts/InstructionsForDestruction.cs
+++ Assets/Scripts/InstructionsForDestruction.cs
@@ -7,25 +7,30 @@ public class InstructionsForDestruction : MonoBehaviour
     Rigidbody2D rb;
     [SerializeField] AudioSource audioSource;
     [SerializeField] AudioClip clip;
-    
+    bool hasPlayed = false;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    void OnCollisionEnter2D(Collision2D other) 
-    {   
-        audioSource.PlayOneShot(clip);
-        Destroy(gameObject , 1.5f);
-        
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (!hasPlayed)
+        {
+            audioSource.PlayOneShot(clip);
+            hasPlayed = true;
+        }
+        Destroy(gameObject, 1.5f);
+
     }
 
- 
+
 }
