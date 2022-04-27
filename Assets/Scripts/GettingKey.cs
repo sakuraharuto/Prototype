bae7c1efc9d8b6939ba3fcb31afa1284f7d5052b
Ruocheng "Sam" Yaoxi <88380883+sakuraharuto@users.noreply.github.com>
2022-04-27 15:52:31 -0400

    Key pickup sound assect and logic

--- Assets/Scripts/GettingKey.cs
+++ Assets/Scripts/GettingKey.cs
@@ -6,10 +6,13 @@ public class GettingKey : MonoBehaviour
 {
     public bool keygot = false;
     public Component key;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] AudioClip clip;
 
     public void OnCollisionEnter2D(Collision2D other) 
     {
         keygot = true;
+        audioSource.PlayOneShot(clip);
         //Destroy(gameObject);
         key.GetComponent<BoxCollider2D>().enabled = false;
         key.GetComponent<SpriteRenderer>().enabled = false;
